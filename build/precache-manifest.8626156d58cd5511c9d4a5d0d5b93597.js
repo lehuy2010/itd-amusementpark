763eefeb4acba783655c8567e1467afcf578ddc1
lehuy2010 <nguyenlehuy1101@gmail.com>
2019-06-04 15:07:16 +0700

    built

---
+++ build/precache-manifest.8626156d58cd5511c9d4a5d0d5b93597.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "a86fcc20257d7b9f7703d1f19f7ec830",
+    "url": "/static/media/vong-luon-tuoi-tho.a86fcc20.jpg"
+  },
+  {
+    "revision": "1ed8a726d9ede4772675",
+    "url": "/static/css/main.a5fe3beb.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c801d30ea55bbf37b2ea6d5a4a904962",
+    "url": "/static/media/tu-avatar.c801d30e.jpg"
+  },
+  {
+    "revision": "5d4c52db41c4e5cf9fe1",
+    "url": "/static/js/2.f538e793.chunk.js"
+  },
+  {
+    "revision": "84d5c5815bad85100fd73b3a22d8458e",
+    "url": "/static/media/image2.84d5c581.jpg"
+  },
+  {
+    "revision": "264cee2a8e064ca3699f7062a1002f1d",
+    "url": "/static/media/image1.264cee2a.jpg"
+  },
+  {
+    "revision": "cc29ba008ccabb0ee9f022b8a81ceb8b",
+    "url": "/static/media/image3.cc29ba00.jpg"
+  },
+  {
+    "revision": "ca21ec3593991bd1195642d9524a602a",
+    "url": "/static/media/bang-dang.ca21ec35.jpg"
+  },
+  {
+    "revision": "051c41d89282c55f1394b10e845fdfd0",
+    "url": "/static/media/ca-chep-nhao-lon.051c41d8.jpg"
+  },
+  {
+    "revision": "c80328f121b7260178e55bcdc7624b4e",
+    "url": "/static/media/cong-vien-khung-long.c80328f1.jpg"
+  },
+  {
+    "revision": "3564a4f1d7ead9f975b7c2d452ea4421",
+    "url": "/static/media/du-quay-dung.3564a4f1.jpg"
+  },
+  {
+    "revision": "1d41c1b534303f00b772f9d0ac6294d4",
+    "url": "/static/media/kid-playgroup.1d41c1b5.jpg"
+  },
+  {
+    "revision": "8851f22e4402609156768dc8575b5269",
+    "url": "/static/media/lau-dai-kinh-di.8851f22e.jpg"
+  },
+  {
+    "revision": "2754857fc9e02af55880077293542b86",
+    "url": "/static/media/me-cung.2754857f.jpg"
+  },
+  {
+    "revision": "34e95a1ffa8cb7fc880eef02a1bd06c1",
+    "url": "/static/media/phuong-hoang-bay.34e95a1f.jpg"
+  },
+  {
+    "revision": "a78fe0ce91c4ea4447b430d5a8d9c488",
+    "url": "/static/media/tau-luon-sieu-toc.a78fe0ce.jpg"
+  },
+  {
+    "revision": "b3fc96fd2d1020868005df35be898131",
+    "url": "/static/media/thuy-cung.b3fc96fd.jpg"
+  },
+  {
+    "revision": "1ed8a726d9ede4772675",
+    "url": "/static/js/main.831b7036.chunk.js"
+  },
+  {
+    "revision": "4a9494132f3762dce4ecf767ae6d4e70",
+    "url": "/static/media/vong-xoay-than-toc.4a949413.jpg"
+  },
+  {
+    "revision": "4b1db082f76c87fd397534d5503e91af",
+    "url": "/static/media/vuon-bach-thu.4b1db082.jpg"
+  },
+  {
+    "revision": "880f6efdbb26041c57f987b446ceb752",
+    "url": "/static/media/vuot-thac.880f6efd.jpg"
+  },
+  {
+    "revision": "0966e5a9380f40e40707c05e064165ae",
+    "url": "/static/media/xe-dien-dung.0966e5a9.jpg"
+  },
+  {
+    "revision": "4ea0ce8f167c3d868e8f3fac549ebfa3",
+    "url": "/static/media/introduction-default.4ea0ce8f.png"
+  },
+  {
+    "revision": "cbeeaad31c3999522aeb9b2b0da8ceb1",
+    "url": "/static/media/introduction-slider-1.cbeeaad3.jpg"
+  },
+  {
+    "revision": "45811e6932b6b41cea67784c9cc918df",
+    "url": "/static/media/introduction-slider-2.45811e69.jpg"
+  },
+  {
+    "revision": "0a24a39271fc90388be78b849d79de6d",
+    "url": "/static/media/introduction-slider-3.0a24a392.jpg"
+  },
+  {
+    "revision": "915cce081aaf43ab8ff280a9823f137b",
+    "url": "/static/media/introduction-slider-4.915cce08.jpg"
+  },
+  {
+    "revision": "f9a5c9f7f004418acd10503a02979879",
+    "url": "/static/media/introduction-slider-5.f9a5c9f7.jpg"
+  },
+  {
+    "revision": "a419b2054173df8611fa4174fc36871e",
+    "url": "/static/media/introduction-slider-6.a419b205.jpg"
+  },
+  {
+    "revision": "94b2b803167dd179c932ae6a5f5325f8",
+    "url": "/static/media/introduction-slider-7.94b2b803.jpg"
+  },
+  {
+    "revision": "41f15a59daae9114a8e51ba2322ee6d3",
+    "url": "/static/media/hiep-avatar.41f15a59.jpg"
+  },
+  {
+    "revision": "5a5d7b6266a35e223fde1a08e0473a54",
+    "url": "/static/media/huy-avatar.5a5d7b62.jpg"
+  },
+  {
+    "revision": "604d050a1cb8e34016fcda2edfd43659",
+    "url": "/static/media/nhi-avatar.604d050a.jpg"
+  },
+  {
+    "revision": "5d4c52db41c4e5cf9fe1",
+    "url": "/static/css/2.ddeb8f76.chunk.css"
+  },
+  {
+    "revision": "1394bbbe2766c80c4614028aa51c4031",
+    "url": "/index.html"
+  }
+];
